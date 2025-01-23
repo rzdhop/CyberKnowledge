5dd144e30ccfc200cac69dc0ce6f012daf419cf1
vRdu <45741890+rzdhop@users.noreply.github.com>
2025-01-23 17:36:07 +0100

    Ajout lancement du process

--- MemPatch/mempatcher.cpp
+++ MemPatch/mempatcher.cpp
@@ -1,13 +1,19 @@
 #include <Windows.h>
+#include <psapi.h>
 #include <iostream>
 #include <vector>
-#include <psapi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <tlhelp32.h>
 #include <string>
 #include <sstream>
+#include <processthreadsapi.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+//CPL cmd > g++ .\mempatcher.cpp -lpsapi -o mempatcher.exe
+
 
 void pretty_print(unsigned char* byte_buffer, size_t byte_buffer_len, uintptr_t startAddress) {
     for (size_t i = 0; i < byte_buffer_len; i++) {
@@ -17,7 +23,8 @@ void pretty_print(unsigned char* byte_buffer, size_t byte_buffer_len, uintptr_t 
                 for (size_t j = i - 16; j < i; j++) {
                     if (isprint(byte_buffer[j])) {
                         printf("%c", byte_buffer[j]);
-                    } else {
+                    }
+                    else {
                         printf(".");
                     }
                 }
@@ -43,7 +50,8 @@ void pretty_print(unsigned char* byte_buffer, size_t byte_buffer_len, uintptr_t 
         for (size_t i = byte_buffer_len - remainder; i < byte_buffer_len; i++) {
             if (isprint(byte_buffer[i])) {
                 printf("%c", byte_buffer[i]);
-            } else {
+            }
+            else {
                 printf(".");
             }
         }
@@ -53,7 +61,7 @@ void pretty_print(unsigned char* byte_buffer, size_t byte_buffer_len, uintptr_t 
 
 int selectProcess() {
     DWORD processIDs[1024], bytesReturned, processCount;
-    
+
     if (!EnumProcesses(processIDs, sizeof(processIDs), &bytesReturned)) {
         printf("Failed to enumerate processes. Error code: %lu\n", GetLastError());
         return -1;
@@ -70,13 +78,15 @@ int selectProcess() {
                 DWORD cbNeeded;
 
                 if (EnumProcessModulesEx(hProcess, &hModule, sizeof(hModule), &cbNeeded, LIST_MODULES_ALL)) {
-                    char processPath[MAX_PATH] = {0};
+                    char processPath[MAX_PATH] = { 0 };
                     if (GetModuleFileNameExA(hProcess, hModule, processPath, MAX_PATH)) {
                         printf("[+] pID: %lu\tPath: %s\n", processIDs[i], processPath);
-                    } else {
+                    }
+                    else {
                         printf("[-] pID: %lu\tCould not retrieve path.\n", processIDs[i]);
                     }
-                } else {
+                }
+                else {
                     printf("[-] pID: %lu\tCould not retrieve process modules.\n", processIDs[i]);
                 }
                 CloseHandle(hProcess);
@@ -88,7 +98,14 @@ int selectProcess() {
     int choice;
     std::cin >> choice;
 
-    return choice;
+    bool goodChoice = false;
+    for (int i = 0; i < processCount && !goodChoice; i++) {
+        if ((DWORD)choice == processIDs[i]) {
+            goodChoice = true;
+        }
+    }
+
+    return goodChoice ? choice : -1;
 }
 
 size_t _dump(DWORD pID, LPCVOID lpBaseAddress, LPVOID lpBuffer, size_t dumpSize) {
@@ -100,10 +117,10 @@ size_t _dump(DWORD pID, LPCVOID lpBaseAddress, LPVOID lpBuffer, size_t dumpSize)
 
     SIZE_T bytesRead = 0;
     BOOL status = ReadProcessMemory(hProcess,
-                                    lpBaseAddress,
-                                    lpBuffer,
-                                    dumpSize,
-                                    &bytesRead);
+        lpBaseAddress,
+        lpBuffer,
+        dumpSize,
+        &bytesRead);
     CloseHandle(hProcess);
 
     if (!status) {
@@ -125,7 +142,7 @@ void dumpMem(DWORD pID) {
     if (startAddr_str.rfind("0x", 0) == 0 || startAddr_str.rfind("0X", 0) == 0) {
         startAddr_str.erase(0, 2);
     }
-    
+
     uintptr_t startAddr = std::stoull(startAddr_str, nullptr, 16);
     LPCVOID lpBaseAddress = reinterpret_cast<LPCVOID>(startAddr);
 
@@ -154,7 +171,7 @@ void patchMem(DWORD pID) {
     // Lecture des données à patcher (ex : "90 90 90")
     printf("Hex bytes to write (e.g. 90 90 90) : ");
     std::cin.ignore();
-    
+
     std::string input;
     std::getline(std::cin, input);
 
@@ -184,65 +201,194 @@ void patchMem(DWORD pID) {
 
     SIZE_T bytesWritten = 0;
     BOOL status = WriteProcessMemory(hProcess,
-                                     lpBaseAddress,
-                                     patchBytes.data(),
-                                     patchBytes.size(),
-                                     &bytesWritten);
+        lpBaseAddress,
+        patchBytes.data(),
+        patchBytes.size(),
+        &bytesWritten);
 
     // Restaurer la protection initiale
     DWORD temp = 0;
     VirtualProtectEx(hProcess, lpBaseAddress, patchBytes.size(), oldProtect, &temp);
-    
+
     if (!status) {
         std::cerr << "WriteProcessMemory failed: " << GetLastError() << std::endl;
-    } else {
-       printf("New Memory :\n");
-       size_t dumpSize = 512;
-       std::vector<unsigned char> bytes_buffer(dumpSize, 0);
+    }
+    else {
+        printf("New Memory :\n");
+        size_t dumpSize = 256;
+        std::vector<unsigned char> bytes_buffer(dumpSize, 0);
 
-       // Lire les nouvelles données pour les afficher
-       size_t actuallyRead = _dump(pID, lpBaseAddress, bytes_buffer.data(), dumpSize);
-       if (actuallyRead > 0) {
-           pretty_print(bytes_buffer.data(), actuallyRead, static_cast<uintptr_t>(patchAddr));
-       }
+        // Lire les nouvelles données pour les afficher
+        size_t actuallyRead = _dump(pID, lpBaseAddress, bytes_buffer.data(), dumpSize);
+        if (actuallyRead > 0) {
+            pretty_print(bytes_buffer.data(), actuallyRead, static_cast<uintptr_t>(patchAddr));
+        }
     }
 
     CloseHandle(hProcess);
 }
 
+bool ResumeProc(DWORD pID) {
+    // https://www.pinvoke.net/default.aspx/ntdll/NtResumeProcess.html
+    // Récupère la fonction NtResumeProcess de ntdll.dll
+    /*
+        NtResumeProcess_t défini par :
+            - NTSTATUS WINAPI <function>(HANDLE){}
+            - <retType> <function> <ArgType>
+        
+        NTSTATUS -> type de retour d'apres le reverse de la fonction
+        WINAPI   -> macro pour __stdcall
+        (WINAPI *) -> defini la fonction
+    */
+
+    using NtResumeProcess_t = NTSTATUS(WINAPI *)(HANDLE);
+    NtResumeProcess_t NtResumeProcess = nullptr; //sera le ptr sur la fonction de ntdll.dll
+
+    HMODULE hNtDll = GetModuleHandleA("ntdll.dll");
+    if (hNtDll) {
+        NtResumeProcess = reinterpret_cast<NtResumeProcess_t>(GetProcAddress(hNtDll, "NtResumeProcess"));
+    }
+    if (!NtResumeProcess) {
+        std::cerr << "Failed to locate NtResumeProcess in ntdll.dll" << std::endl;
+        exit(1);
+    }
+
+    // Ouvrir un handle au processus cible
+    HANDLE hProcess = OpenProcess(PROCESS_SUSPEND_RESUME, FALSE, pID);
+    if (!hProcess) {
+        std::cerr << "Failed to open process. Error: " << GetLastError() << std::endl;
+        return false;
+    }
+
+    // Resumer tous les threads du processus
+    BOOL status = NtResumeProcess(hProcess);
+    CloseHandle(hProcess);
+
+    if (status != 0) {
+        std::cerr << "Failed to resume process. Error: " << GetLastError() << std::endl;
+        return false;
+    }
+
+    printf("Process with PID %lu resumed successfully.\n", pID);
+    return true;
+}
+
+
+int CreateProc() {
+    printf("Enter path to executable : \n> ");
+    std::cin.ignore();
+    std::string input;
+    std::getline(std::cin, input);
+
+    printf("Enter args (optional | separated by space) : \n> ");
+    std::cin.ignore();
+    std::string args;
+    std::getline(std::cin, args);
+    
+    // Maybe One day : option to use as another user (user token (e.g: runas...))
+    // use of CreateProcessAsUserA & LogonUserA
+
+    if (!fs::exists(input)) {
+        return -1;
+    }
+    STARTUPINFOA si = { 0 }; 
+    PROCESS_INFORMATION pi = { 0 };
+    si.cb = sizeof(STARTUPINFOA);
+
+    // Combiner le chemin de l'exécutable et ses arguments
+    std::string cmdLine = "\"" + input + "\" " + args;
+
+    BOOL result = CreateProcessA(
+        input.c_str(),                        
+        cmdLine.data(),
+        nullptr,          // Pas de sécurité pour le processus
+        nullptr,          // Pas de sécurité pour le thread
+        FALSE,            // Héritage des handles non permis
+        CREATE_SUSPENDED | CREATE_NEW_CONSOLE,
+        nullptr,          // Variables d'environnement (null = hérite)
+        nullptr,          // Répertoire de travail courant
+        &si,              // Informations sur la fenêtre
+        &pi               // Informations sur le processus
+    );
+
+    if (!result) {
+        std::cerr << "Failed to create process. Error: " << GetLastError() << std::endl;
+        return -1;
+    }
+
+    printf("Process created in suspended mode. PID: %lu\n", pi.dwProcessId);
+
+    // Close handles on processInfo
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+
+    return pi.dwProcessId;
+}
+
 int main(int argc, char** argv) {
-    DWORD pID = selectProcess();
+    int choice;
+    DWORD pID = -1;
+
+    printf("+----vRdu's MemPatcher----+\n");
+    printf("|                         |\n");
+    printf("| Patch:                  |\n");
+    printf("|    1) Create proces     |\n");
+    printf("|    2) Attach proces     |\n");
+    printf("|    0) Exit              |\n");
+    printf("|                         |\n");
+    printf("+-- Choice: ");
+    std::cin >> choice;
+
+    switch (choice) {
+    case 1:
+        pID = CreateProc();
+        break;
+    case 2:
+        pID = selectProcess();
+        break;
+    case 0:
+        printf("\n[-]Bye !");
+        return 0;
+    default:
+        printf("Unknown choice!\n");
+        break;
+    }
+
     if (pID == -1) {
         return 0;
     }
 
     bool loop = true;
-    int choice;
 
-    while(loop) {
+    while (loop) {
         printf("+----vRdu's MemPatcher----+\n");
         printf("|                         |\n");
-        printf("| Menu:                   |\n");
+        printf("| Menu (pID %lu):         |\n", pID);
         printf("|    1) dumMem            |\n");
         printf("|    2) patchMem          |\n");
+        printf("|    3) ResumeProc        |\n");
         printf("|    0) Exit              |\n");
         printf("|                         |\n");
         printf("+-- Choice: ");
         std::cin >> choice;
 
-        switch(choice) {
-            case 1:
-                dumpMem(pID);
-                break;
-            case 2:
-                patchMem(pID);
-                break;
-            case 0:
-                loop = false;
-                break;
-            default:
-                printf("Unknown choice!\n");
-                break;
+        switch (choice) {
+        case 1:
+            dumpMem(pID);
+            break;
+        case 2:
+            patchMem(pID);
+            break;
+        case 3:
+            ResumeProc(pID);
+            break;
+        case 0:
+            printf("\n[-]Bye !");
+            loop = false;
+            break;
+        default:
+            printf("Unknown choice!\n");
+            break;
         }
     }
 
