1f19f0aed89ad8a04434b95e897424eee0c3a9d8
vRdu <45741890+rzdhop@users.noreply.github.com>
2025-05-04 18:27:53 +0200

    on perd pas de temps

---
+++ asbel/CallbackEnum/enumClient/enumClient.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+
+namespace callbackEnum {
+    namespace ioctl_codes {
+        constexpr ULONG processCallbackEnum = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x501, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+    }
+
+    struct REQUEST {
+        HANDLE process; //process attached
+
+        PVOID target; //Mem target addrr
+        PVOID buffer; //Mem content
+
+        SIZE_T buffer_size;
+        SIZE_T return_size;
+    };
+}
+
+DWORD get_process_id(const wchar_t* process_name)
+{
+    DWORD  pid = 0;
+    HANDLE snap_shot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (snap_shot == INVALID_HANDLE_VALUE)
+        return 0;
+
+    PROCESSENTRY32W entry = { 0 };
+    entry.dwSize = sizeof(entry);
+
+    if (Process32FirstW(snap_shot, &entry) == TRUE)
+    {
+        do
+        {
+            if (_wcsicmp(process_name, entry.szExeFile) == 0) //wide string cmp
+            {
+                pid = entry.th32ProcessID;
+                break;
+            }
+        } while (Process32NextW(snap_shot, &entry) == TRUE);
+    }
+    else
+    {
+        printf("Process32FirstW failed (err=%lu)\n", GetLastError());
+    }
+
+    CloseHandle(snap_shot);
+    return pid;
+}
+
+/* Renvoie l’adresse de base (DWORD_PTR) d’un module précis dans un process donné.
+   0 → module introuvable / erreur. */
+DWORD_PTR get_module_base(DWORD pid, const wchar_t* module_name)
+{
+    DWORD_PTR module_base = 0;
+
+    /* snapshot limitée au process ciblé */
+    HANDLE snap_shot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+    if (snap_shot == INVALID_HANDLE_VALUE)
+        return 0;
+
+    MODULEENTRY32W entry = { 0 };
+    entry.dwSize = sizeof(entry);
+
+    if (Module32FirstW(snap_shot, &entry) == TRUE)
+    {
+        do
+        {
+            if (_wcsicmp(module_name, entry.szModule) == 0)
+            {
+                module_base = (DWORD_PTR)entry.modBaseAddr;   /* adresse de base   */
+                break;
+            }
+        } while (Module32NextW(snap_shot, &entry) == TRUE);
+    }
+    else
+    {
+        printf("Module32FirstW failed (err=%lu)\n", GetLastError());
+    }
+
+    CloseHandle(snap_shot);
+    return module_base;
+}
+
+int main() {
+
+	return 0;
+}
---
+++ asbel/SSDTEnum/SSDTEnum/SSDTEnumerator.cpp
@@ -0,0 +1,51 @@
+#include <ntifs.h>
+#include <intrin.h>
+#pragma intrinsic(__readmsr)
+
+#define LSTAR 0xC0000082
+
+typedef struct _KSERVICE_DESCRIPTOR_TABLE
+{
+    PULONG_PTR ServiceTable;
+    ULONG_PTR Reserved0;     // padding ? 
+    ULONG ServiceLimit;
+    PULONG_PTR ArgumentTable;
+} KSDT, * PKSDT;
+
+
+VOID DriverUnload(PDRIVER_OBJECT DriverObject) {
+    UNREFERENCED_PARAMETER(DriverObject);
+    DbgPrint("[SSDT Enum] Bye !\n");
+}
+
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(RegistryPath);
+    DbgPrint("[SSDT Enum] Hi !\n");
+
+    PUCHAR KiSystemCall64 = (PUCHAR)__readmsr(0xC0000082);
+    DbgPrint("[SSDT Enum] LSTAR at : %p \n", KiSystemCall64);
+
+    for (int i = 0; i < 0x500; i++) {
+        if (*(KiSystemCall64 + i) == 0x4c && *(KiSystemCall64 + (i + 1)) == 0x8d && *(KiSystemCall64 + (i + 2)) == 0x15) {
+            int offset = *(int*)(KiSystemCall64 + (i + 3));
+            PKSDT KeServiceDescriptorTable = (PKSDT)(KiSystemCall64 + i + 7 + offset); //Post instruction's opcode
+            DbgPrint("[SSDT Enum] KeServiceDescriptorTable at : %p \n", KeServiceDescriptorTable);
+
+            PULONG_PTR KiServiceTable = KeServiceDescriptorTable->ServiceTable;
+            ULONG NbEntry = KeServiceDescriptorTable->ServiceLimit;
+            if (!MmIsAddressValid(KiServiceTable)) break;
+            DbgPrint("[SSDT Enum] KiServiceTable at : %p \n", KiServiceTable);
+
+            for (int x = 0; x < (int)NbEntry; x++) {
+                UINT32 offset_SSDT = ((int*)KiServiceTable)[x];
+                void* function = (PUCHAR)KiServiceTable + (offset_SSDT >> 4);
+                DbgPrint("[SSDT Enum] Index %d : %p\n", x, function);
+            }
+            break;
+        }
+    }
+
+    DriverObject->DriverUnload = DriverUnload;
+    return STATUS_SUCCESS;
+}
